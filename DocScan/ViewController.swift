649ff273721b22c3b7eb48f156392d6d65cf6f9d
Ankit Yadav <yadavankit840@gmail.com>
2020-10-15 14:17:45 +0530

    Initial Commit

---
+++ DocScan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DocScan
+//
+//  Created by Ankit on 15/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
